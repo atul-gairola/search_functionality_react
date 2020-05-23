40ce55d08d7d914592adefd895d59d92b8f6ffe1
Atul Gairola <atulgairola08@gmail.com>
2020-05-23 11:07:10 +0530

    done

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ const PORT = process.env.PORT || 5000;
 
 // for deployment
 
-    app.use(express.static('./client/build'));
+    app.use(express.static( __dirname + '/client/build'));
 
     app.get('*', (req, res) => {
      res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
