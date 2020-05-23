8796a6f50df86a79287cec3c4e667e039bc2ceca
Atul Gairola <atulgairola08@gmail.com>
2020-05-23 11:05:18 +0530

    done

--- server.js
+++ server.js
@@ -30,13 +30,13 @@ console.log('Body: ', inputMovieName);
 const PORT = process.env.PORT || 5000;
 
 // for deployment
-if(process.env.NODE_ENV === 'production'){
+
     app.use(express.static('./client/build'));
 
     app.get('*', (req, res) => {
      res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
     })
-}
+
 
 app.listen(PORT, () => {
     console.log(`Server active on port ${PORT}`);
